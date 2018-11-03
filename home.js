f4a38171269978f7b29036e5768869f0c91f59f8
justvuit <benjamin_vu@brown.edu>
2018-11-02 20:03:33 -0400

    First edition

---
+++ home.js
@@ -0,0 +1,26 @@
+$(function() {
+  $('#dinner').hover(function() {
+    $('#title').css('text-shadow', '13px 7px 2px #A3D5A8, 12px 6.5px 2px #A3D5A8, 12px 6px 2px #A3D5A8, 10px 5.5px 6px #A3D5A8, 9px 5px 2px #A3D5A8, 8px 4.5px 2px #A3D5A8, 7px 4px 2px #A3D5A8, 6px 3.5px 2px #A3D5A8, 5px 3px 2px #A3D5A8, 4px 2.5px 2px #A3D5A8, 3px 2px 2px #A3D5A8, 2px 1.5px 2px #A3D5A8, 1px 1px 2px #A3D5A8');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#title').css('text-shadow', '13px 7px 2px #AFE0EF, 12px 6.5px 2px #AFE0EF, 12px 5px 2px #AFE0EF, 10px 5.5px 2px #AFE0EF, 9px 5px 2px #AFE0EF, 8px 4.5px 2px #AFE0EF, 7px 4px 2px #AFE0EF, 6px 3.5px 2px #AFE0EF, 5px 3px 2px #AFE0EF, 4px 2.5px 2px #AFE0EF, 3px 2px 2px #AFE0EF, 2px 1.5px 2px #AFE0EF, 1px 1px 2px #AFE0EF');
+  });
+});
+
+$(function() {
+  $('#breakfast').hover(function() {
+    $('#title').css('text-shadow', '13px 7px 2px #99C4CE, 12px 6.5px 2px #99C4CE, 12px 6px 2px #99C4CE, 10px 5.5px 2px #99C4CE, 9px 5px 2px #99C4CE, 8px 4.5px 2px #99C4CE, 7px 4px 2px #99C4CE, 6px 3.5px 2px #99C4CE, 5px 3px 2px #99C4CE, 4px 2.5px 2px #99C4CE, 3px 2px 2px #99C4CE, 2px 1.5px 2px #99C4CE, 1px 1px 2px #99C4CE');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#title').css('text-shadow', '13px 7px 2px #AFE0EF, 12px 6.5px 2px #AFE0EF, 12px 5px 2px #AFE0EF, 10px 5.5px 2px #AFE0EF, 9px 5px 2px #AFE0EF, 8px 4.5px 2px #AFE0EF, 7px 4px 2px #AFE0EF, 6px 3.5px 2px #AFE0EF, 5px 3px 2px #AFE0EF, 4px 2.5px 2px #AFE0EF, 3px 2px 2px #AFE0EF, 2px 1.5px 2px #AFE0EF, 1px 1px 2px #AFE0EF');
+  });
+});
+
+$(function() {
+  $('#lunch').hover(function() {
+    $('#title').css('text-shadow', '13px 7px 2px #F2CF6D, 12px 6.5px 2px #F2CF6D, 12px 6px 2px #F2CF6D, 10px 5.5px 2px #F2CF6D, 9px 5px 2px #F2CF6D, 8px 4.5px 2px #F2CF6D, 7px 4px 2px #F2CF6D, 6px 3.5px 2px #F2CF6D, 5px 3px 2px #F2CF6D, 4px 2.5px 2px #F2CF6D, 3px 2px 2px #F2CF6D, 2px 1.5px 2px #F2CF6D, 1px 1px 2px #F2CF6D');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#title').css('text-shadow', '13px 7px 2px #AFE0EF, 12px 6.5px 2px #AFE0EF, 12px 5px 2px #AFE0EF, 10px 5.5px 2px #AFE0EF, 9px 5px 2px #AFE0EF, 8px 4.5px 2px #AFE0EF, 7px 4px 2px #AFE0EF, 6px 3.5px 2px #AFE0EF, 5px 3px 2px #AFE0EF, 4px 2.5px 2px #AFE0EF, 3px 2px 2px #AFE0EF, 2px 1.5px 2px #AFE0EF, 1px 1px 2px #AFE0EF');
+  });
+});
